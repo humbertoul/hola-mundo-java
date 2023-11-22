6a85003d6bec7b30fa5005a1309d02d352156e48
Humberto Barrantes <maikol.barrantes@ulatina.net>
2023-11-21 19:21:45 -0600

    Ciclo for

--- HolaMundo/src/Main.java
+++ HolaMundo/src/Main.java
@@ -2,5 +2,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hola saulo");
+
+        int x = 0;
+        for (x=20; x<100; x++){
+            System.out.println(x);
+        }
+
     }
 }
