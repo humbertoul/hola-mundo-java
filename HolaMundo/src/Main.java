a064d62542a2c6b4f19c5c20d2d258616a3c7fc0
SauloRod21 <saulorodriguezchaverri@gmail.com>
2023-11-21 19:20:58 -0600

    Se agrego una suma

--- HolaMundo/src/Main.java
+++ HolaMundo/src/Main.java
@@ -2,5 +2,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hola saulo");
+
+
+      int y = 10;
+      int x = 5;
+
+        System.out.println(x - y);
     }
 }
